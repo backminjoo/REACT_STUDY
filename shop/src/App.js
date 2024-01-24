cf2ec203630c13ce2da2851f45fc40377cf250c6
backminjoo <qoralswn1205@daum.net>
2024-01-24 18:01:39 +0900

    shop 14강

---
+++ shop/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Minjoo's shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'></div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+
+          <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+
+          <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
