61c5448198bf904165548c23c8abcb80be91e1d6
backminjoo <qoralswn1205@daum.net>
2024-01-27 18:05:14 +0900

    0127 react study

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,12 +3,12 @@ import {Button, Navbar, Container, Nav} from 'react-bootstrap';
 import './App.css';
 import { useState } from 'react';
 import data from './data.js';
-import{Routes,Route,Link} from 'react-router-dom'
-
+import{Routes,Route,Link, useNavigate, Outlet} from 'react-router-dom'
+import Detail from './routes/Detail.js';
 function App() {
 
   let [shoes] = useState(data);
-
+  let navigate = useNavigate();
   return (
     <div className="App">
 
@@ -21,13 +21,13 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Minjoo's shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/ ')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
 
           </Nav>
         </Container>
       </Navbar>
-
+      
       {/* <Link to="/">홈</Link>
       <Link to="/detail">상세페이지</Link> */}
 
@@ -54,13 +54,27 @@ function App() {
                 </>
         }/>
 
-        <Route path="/detail" element={<div>상세페이지임</div>}/>
+        <Route path="/detail" element={<Detail/>}/>
+
+        <Route path="*" element={<div>없는페이지입니다.</div>}/>
+
+        <Route path="/about" element={<About/>}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>위치정보임</div>} />
+        </Route>
+
       </Routes>
-
-
-
     </div>
   );
+}
+
+function About(){
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 function Card(props) {
